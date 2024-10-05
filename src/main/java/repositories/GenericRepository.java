0daa8b27c70550ed84312cf1a07c50ddee042e10
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-10-05 10:57:59 -0300

    feat(Repositories): Add a generic repository
    Add the interface GenericRepositoryInterface.
    
    Add the class GenericRepository.

---
+++ src/main/java/repositories/GenericRepository.java
@@ -0,0 +1,62 @@
+package repositories;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+
+import repositories.interfaces.GenericRepositoryInterface;
+
+public class GenericRepository<T, ID> implements GenericRepositoryInterface<T, ID> {
+    private final Class<T> entityClass;
+    private final SessionFactory sessionFactory;
+
+    public GenericRepository(Class<T> entityClass, SessionFactory sessionFactory) {
+        this.entityClass = entityClass;
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Override
+    public void save(T entity) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(entity);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public T findById(ID id) {
+        try (Session session = sessionFactory.openSession()) {
+            return session.get(entityClass, id);
+        }
+    }
+
+    @Override
+    public List<T> findAll() {
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("from " + entityClass.getName(), entityClass).list();
+        }
+    }
+
+    @Override
+    public void delete(ID id) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            T entity = session.get(entityClass, id);
+            if (entity != null) {
+                session.remove(entity);
+            }
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/repositories/interfaces/GenericRepositoryInterface.java
@@ -0,0 +1,10 @@
+package repositories.interfaces;
+
+import java.util.List;
+
+public interface GenericRepositoryInterface<T, ID> {
+    void save(T entity);
+    T findById(ID id);
+    List<T> findAll();
+    void delete(ID id);
+}
