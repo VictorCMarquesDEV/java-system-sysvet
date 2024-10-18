8c203d8e8f32ff1e36280b4652157d5a14abf8d7
David Rios <55660690+davidrioss@users.noreply.github.com>
2024-10-18 10:46:38 -0300

    tela login

---
+++ src/main/java/LoginController.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class LoginController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("login");
+    }
+}
