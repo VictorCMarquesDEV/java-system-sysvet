9d25d177e59e1e71c77fcde52dbc7231497ec602
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-10-19 09:40:54 -0300

    feat(Models): add a new model
    Add the model that represents the employee.

---
+++ src/main/java/models/Funcionario.java
@@ -0,0 +1,61 @@
+package models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "funcionarios")
+public class Funcionario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "nome",nullable = false)
+    private String nome;
+    
+    @Column(name = "cpf",unique = true,nullable = false)
+    private String cpf;
+
+    @Column(name = "salario",nullable = false)
+    private double salario;
+
+    public Funcionario(){}
+
+    public Funcionario(String nome, String cpf, double salario){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = salario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return nome;
+    }
+
+    public void setName(String nome){
+        this.nome = nome;
+    }
+
+    public String getCPF() {
+        return cpf;
+    }
+
+    public void setCPF(String cpf){
+        this.cpf = cpf;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+}
