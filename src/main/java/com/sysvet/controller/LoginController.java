ce566c79bd57f5f54239c197d42d6c9c0301e21d
David Rios <55660690+davidrioss@users.noreply.github.com>
2024-10-24 22:45:21 -0300

    Alerta de campos obrigatórios no login
    adicionada função para exibir alerta de campos obrigatórios no login

--- src/main/java/com/sysvet/controller/LoginController.java
+++ src/main/java/com/sysvet/controller/LoginController.java
@@ -3,12 +3,36 @@ package com.sysvet.controller;
 import java.io.IOException;
 import com.sysvet.App;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class LoginController {
 
     @FXML
-    private void switchToInicio() throws IOException {
+    private TextField loginUser;      // Referência ao campo de texto do usuário
+    @FXML
+    private PasswordField loginPassword; // Referência ao campo de senha
+
+    
+    @FXML
+    private void validateLogin() throws IOException {
+        
+        if (loginUser.getText().isEmpty() || loginPassword.getText().isEmpty()) {
+            showAlert("Campos obrigatórios", "Por favor, preencha todos os campos.");
+            return; // Interrompe a execução se os campos estiverem vazios
+        }        
+        
         App.setRoot("/view/inicio");
+    }
+
+     // Método para mostrar um alerta
+     private void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.WARNING);
+        alert.setTitle(title);
+        alert.setHeaderText(null); // Sem cabeçalho
+        alert.setContentText(message);
+        alert.showAndWait(); // Exibe o alerta e aguarda até que seja fechado
     }
     
 }
