0c7c4397a0fbd83745a20d404984e8071b6ce3a0
vitor0p9f <dev.vitorpfontes002@gmail.com>
2024-10-29 18:00:25 -0300

    feat(App): update the App.java file
    Initialize the Hibernate session factory.

--- src/main/java/com/sysvet/App.java
+++ src/main/java/com/sysvet/App.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+import utils.hibernateSessionFactorySingleton;
 import java.io.IOException;
 
 /**
@@ -55,7 +55,9 @@ public class App extends Application {
     }
 
     public static void main(String[] args) {
-        launch();
+        new hibernateSessionFactorySingleton();
+        
+        launch();        
     }
 
 }
