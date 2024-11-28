b218eb7ae401e8bec956d1d9b010599c8b2840d7
Nicolle <dev.marianicolle@gmail.com>
2024-11-28 13:02:36 -0300

    icones de del e edit medicamentos

--- src/main/java/repositories/medicamentoRepository.java
+++ src/main/java/repositories/medicamentoRepository.java
@@ -1,10 +1,15 @@
 package repositories;
 
 import exceptions.GenericException;
+import models.Funcionario;
 import models.Medicamentos;
+import models.Usuarios;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
-public class medicamentoRepository extends GenericRepository<Medicamentos, Long>{
+
+public class medicamentoRepository extends GenericRepository<Medicamentos, Long> {
 
     public medicamentoRepository(SessionFactory sessionFactory) {
         super(Medicamentos.class, sessionFactory);
@@ -15,4 +20,17 @@ public class medicamentoRepository extends GenericRepository<Medicamentos, Long>
         // Executa o save padrão definido no GenericRepository
         super.save(entity);
     }
+    public void delete(Long id) {
+        try (Session session = sessionFactory.openSession()) {
+            Transaction tx = session.beginTransaction();
+            Medicamentos medicamento = session.find(Medicamentos.class, id);
+            if (medicamento != null) {
+                session.delete(medicamento);
+            }
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
